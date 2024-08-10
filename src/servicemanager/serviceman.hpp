8fa039c2a26a31d2b15b5eb08a978cd78a4ac2c8
KnoxaramaV2 <woodrowtpscott@gmail.com>
2024-08-10 18:51:53 -0500

    Updated makefiles

--- src/servicemanager/serviceman.hpp
+++ src/servicemanager/serviceman.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <unordered_map>
+#include <map>
 #include <typeindex>
 #include <typeinfo>
 #include <memory>
--- src/test/service_tests.cpp
+++ src/test/service_tests.cpp
@@ -9,39 +9,6 @@ void testCrossResolves();
 
 serviceman::ServiceManager __servMan;
 
-struct IDataType{
-    const char* _msgType;
-    int loc;
-    IDataType(): IDataType("IDefaultType"){};
-    IDataType(const char* msg): _msgType(msg){
-        static int sValue = 1;
-        loc = sValue++;
-        printf("+ %s(%d) @ %lx\n", _msgType, loc, appId(this));
-    }
-    ~IDataType(){ printf("- %s(%d) @ %lx\n", _msgType, loc, appId(this)); }
-
-};
-
-struct Singelton1 : public IDataType{
-    Singelton1(): IDataType("SINGELTON1"){}
-};
-
-struct Singelton2 : public IDataType{
-    Singelton2(): IDataType("SINGELTON2"){}
-};
-
-// struct Scoped: public DataType{
-//     Scoped(): DataType("SCOPED"){}
-// };
-
-struct Instance1: public IDataType{
-    Instance1(): IDataType("INSTANCE1"){}
-};
-
-struct Instance2: public IDataType{
-    Instance2(): IDataType("INSTANCE2"){}
-};
-
 struct NoRegister{};
 
 void initFixtures(){
@@ -66,33 +33,13 @@ int main(){
 
 
 void testInstance(){
-    assert((__servMan.registerInstance<IDataType, Instance1>()));
-    assert((__servMan.registerInstance<Instance2>()));
 
-    assertThrows(__servMan.resolveServiceInstance<NoRegister>());
-
-    assertNotThrows(__servMan.resolveServiceInstance<Instance1>());
-    assertNotThrows(__servMan.resolveServiceInstance<Instance2>());
-
-    assertThrows(__servMan.resolveServiceInstance<IDataType>());
 }
 
 void testSingelton(){
 
-    assert((__servMan.registerSingelton<IDataType, Singelton1>()));
-    assert((__servMan.registerSingelton<Singelton2>()));
-
-    assertThrows((__servMan.resolveServiceInstance<Singelton1>()));
-    assertThrows((__servMan.resolveServiceSingelton<Instance1>()));
-
-    assertThrows(__servMan.resolveServiceSingelton<NoRegister>());
-
-    assertNotThrows(__servMan.resolveServiceSingelton<Singelton1>());
-    assertNotThrows(__servMan.resolveServiceSingelton<Singelton2>());
 }
 
 void testCrossResolves(){
-    assertThrows(__servMan.resolveServiceSingelton<Instance1>());
-    assertThrows(__servMan.resolveServiceInstance<Singelton1>());
 }
 
