bd848dfceec19b0fcb9fc44ccd982be6b24676b6
KnoxaramaV2 <woodrowtpscott@gmail.com>
2024-08-13 13:13:46 -0500

    Redefined type constructors

--- src/dumisdk/dtypes.cpp
+++ src/dumisdk/dtypes.cpp
@@ -1,4 +1,5 @@
 #include "dtypes.hpp"
+#include "defs.hpp"
 
 #include <type_traits>
 #include <stdexcept>
@@ -6,3 +7,24 @@
 #include <vector>
 #include <map>
 
+dumisdk::DCMemObj::DCMemObj(DCTypes type):id(appId(this)),type(type)
+{
+    _value = initTypeDefault(type);
+}
+
+void *dumisdk::initTypeDefault(DCTypes type)
+{
+    switch (type)
+    {
+    case DCTypes::DCBOOL: return new DCBOOL_TYPE(getTypeDefault<DCBOOL_TYPE>());
+    case DCTypes::DCINT: return new DCINT_TYPE(getTypeDefault<DCINT_TYPE>());
+    case DCTypes::DCDECI: return new DCDECI_TYPE(getTypeDefault<DCDECI_TYPE>());
+    case DCTypes::DCSTR: return new DCSTRING_TYPE(getTypeDefault<DCSTRING_TYPE>());
+    case DCTypes::DCLIST: return new DCLIST_TYPE(getTypeDefault<DCLIST_TYPE>());
+    case DCTypes::DCMAP: return new DCMAP_TYPE(getTypeDefault<DCMAP_TYPE>());
+    
+    default:
+        break;
+    }
+    throw dumiexception("Illegal DC type code");
+}
--- src/dumisdk/dtypes.hpp
+++ src/dumisdk/dtypes.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "defs.hpp"
+#include "dumiexcept.hpp"
 
 #include <string>
 #include <vector>
@@ -13,16 +14,42 @@ namespace dumisdk
         DCBOOL, DCINT, DCDECI,
         DCSTR, DCMAP, DCLIST
     };
-    
+
+    template<typename T>
+    T defaultTypeValue();
+
+    struct DCMemObj{
+        const APPSID id;
+        const DCTypes type;
+        
+        DCMemObj(DCTypes type);
+
+        protected:
+        void* _value;
+    };
+
     typedef bool DCBOOL_TYPE;
     typedef long DCINT_TYPE;
     typedef double DCDECI_TYPE;
     typedef std::string DCSTRING_TYPE;
-    template<typename T>
-    using DCLIST_TYPE = std::vector<T>;
-    template<typename T>
-    using DCMAP_TYPE = std::map<HASHID, T>;
+    typedef std::vector<DCMemObj*> DCLIST_TYPE;
+    typedef std::map<APPSID, DCMemObj*> DCMAP_TYPE;
 
+    template<typename T>
+    T getTypeDefault(){
+        static_assert(
+            std::is_same<T, DCBOOL_TYPE>::value ||
+            std::is_same<T, DCINT_TYPE>::value ||
+            std::is_same<T, DCDECI_TYPE>::value ||
+            std::is_same<T, DCSTRING_TYPE>::value ||
+            std::is_same<T, DCMAP_TYPE>::value ||
+            std::is_same<T, DCLIST_TYPE>::value  
+            ,"Non DCType default requested"
+        );
+        return T();
+    };
+
+    void* initTypeDefault(DCTypes type);
     
-};
 
+};
